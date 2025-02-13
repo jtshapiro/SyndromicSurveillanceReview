3c86d4ec4943f841b5e8ed2938cfe759da03d1cc
Julie Teresa Shapiro <julie.teresa.shapiro@gmail.com>
2025-02-13 15:52:06 +0100

    Updated scripts and data following manuscript revision
    Updated scripts and data following manuscript revision for Preventive Veterinary Medicine.
    Additional columns have been added to dat.csv
    Multiple-factor analysis has been removed
    Other minor changes

--- CurrentPracticeFutureDirectionsSyndromicSurveillance.R
+++ CurrentPracticeFutureDirectionsSyndromicSurveillance.R
@@ -1,9 +1,12 @@
 ########################################################################################
 # Code to perform analysis and recreate in 
 # Current Practice and Future Directions in Syndromic Surveillance for Animal Health: 
-# A Systematic Review and Analysis
+# A Scoping Review and Analysis
 #
 # Script tested for R version 4.1.2
+#
+# Script updated 13 February 2025
+# following manuscript revisions
 ########################################################################################
 
 ########################################################################################
@@ -25,18 +28,19 @@ library(factoextra)
 library(ggpubr)
 
 # Read in the data:
-dat <- read.csv("dat.csv", header=T, sep = ",")
+dat <- read_csv("dat.csv", header=T)
 
 ########################################################################################
 # Unit of Analysis
 ########################################################################################
-# Papers per Unit of analysis (individual, herd/flock, population)
+# Summarize the number of papers per unit of analysis (individual, herd/flock, population)
 unit.analysis <- dat %>%
+  filter(!(Source=="Gray literature")) %>%
   distinct(Title, .keep_all = T) %>%
   group_by(Unit_analysis) %>%
   summarise(num.articles=sum(n()))
 
-# Filter papers analyzing data at the individual level:
+# Filter papers analyzing data at the individual level for downstream analysis:
 no.ind <- dat %>%
   filter(!(Unit_analysis=="Individual"))
 
@@ -45,15 +49,18 @@ no.ind <- dat %>%
 ########################################################################################
 # Plot the number of publications per year (Figure 2) 
 # Note: Excludes papers analyzing at the individual level
+# Select relevant columns
 dat.bar.chart <- no.ind %>%
   select(Title,Year, Authors, Journal, DOI)
 
-# Barchart of original data only
+# Summarize number of papers per year
 papers.per.year <- dat.bar.chart %>%
+  filter(!(Source=="Gray literature")) %>%
   distinct(Title, .keep_all=TRUE) %>%
   group_by(Year)%>%
   summarise(num.articles=sum(n()))
 
+#Create barchart of papers per year (Fig. 2)
 paper.plot <- ggplot(papers.per.year, aes(y=num.articles, 
                                           x=Year)) + 
   geom_bar(position="stack", stat="identity", fill="light grey", color="black") +
@@ -74,17 +81,18 @@ paper.plot <- ggplot(papers.per.year, aes(y=num.articles,
 # Identify and separate named systems
 # Assign a unique ID number to each system
 named <- no.ind %>%
-  filter(!is.na(Surveillance_system_name)) %>%
-  group_by(Surveillance_system_name) %>%
+  filter(!is.na(Surveillance_system)) %>%
+  filter(!(Surveillance_system=="NA")) %>%
+  group_by(Surveillance_system) %>%
   mutate(syst_id = cur_group_id()) %>%
   select(syst_id, everything())
 
 # Give an ID to each unnamed system
 dat.analysis <- no.ind %>%
-  filter(is.na(Surveillance_system_name)) %>%
+  filter(Surveillance_system =="NA"| Surveillance_system=="NA") %>%
   group_by(Title) %>%
   mutate(syst_id = cur_group_id()) %>%
-  mutate(syst_id = syst_id + 60) %>%
+  mutate(syst_id = syst_id + 57) %>%
   select(syst_id, everything()) %>%
   ungroup() %>%
   # Bind named systems
@@ -115,10 +123,11 @@ dat.analysis2 <- dat.analysis %>%
 
 # Calculate number of articles per system
 arts.per.syst <- dat.analysis2 %>%
+  filter(!(Source=="Gray literature")) %>%
   group_by(syst_id) %>%
   summarise(num.articles=sum(n()))
 
-# Now, keep only 1 SyS per ID
+# Now, keep only 1 row of data per system (ID)
 keep.syst.ana <- dat.analysis2 %>%
   filter(Keep=="Yes")
 
@@ -131,11 +140,8 @@ keep.unique.title <- keep.syst.ana %>%
 
 doub.arts <- anti_join(keep.syst.ana, keep.unique.title)
 
-add.back <- doub.arts %>%
-  filter(syst_id != 36)
-
 keep.syst.diff.ind.spp <- keep.unique.title %>%
-  bind_rows(add.back)
+  bind_rows(doub.arts)
 
 ########################################################################################
 # Descriptive statistics of systems, by single variable
@@ -144,24 +150,54 @@ keep.syst.diff.ind.spp <- keep.unique.title %>%
 summ.type <- keep.unique.title %>%
   group_by(Surveillance_Type) %>%
   summarise(n = sum(n())) %>%
-  mutate(percent=(n/131)*100) %>%
+  mutate(percent=(n/126)*100) %>%
+  arrange(., desc(percent))
+
+# Governance
+summ.governance <- keep.unique.title %>%
+  filter(Surveillance_Type=='Established'|Surveillance_Type=='Temporary' ) %>%
+  group_by(Governance) %>%
+  summarise(n = sum(n())) %>%
+  mutate(percent=(n/26)*100) %>%
+  arrange(., desc(percent))
+
+# Objectives
+summ.obj <- keep.unique.title %>%
+  group_by(System_Purpose) %>%
+  summarise(n = sum(n())) %>%
+  mutate(percent=(n/126)*100) %>%
+  arrange(., desc(percent))
+
+# Response
+summ.resp <- keep.unique.title %>%
+  filter(!(Surveillance_Type == "Prospective/Proof of Concept")) %>%
+  group_by(Response_alarm) %>%
+  summarise(n = sum(n())) %>%
+  mutate(percent=(n/42)*100) %>%
   arrange(., desc(percent))
 
 # Geographic scale
 summ.geoscale <- keep.unique.title %>%
   group_by(Geographic_Extent) %>%
   summarise(n=n()) %>%
-  mutate(percent=n/131*100) %>%
+  mutate(percent=n/126*100) %>%
   arrange(desc(percent))
 
-# Summarise by continent + count by continent
+# Summarise by continent
 summ.cont <- keep.unique.title %>%
   group_by(Region.x) %>%
   summarise(n=n()) %>%
-  mutate(percent=n/131*100) %>%
+  mutate(percent=n/126*100) %>%
   arrange(desc(percent))
 
-# Animal sectors and species
+# OneHealth systems
+summ.oh <- keep.unique.title %>%
+  group_by(Integration_HumanHealth) %>%
+  summarise(n=n()) %>%
+  mutate(percent=n/126*100) %>%
+  arrange(desc(percent))
+
+# Summarize by animal sectors and species
 # For animal sector continue to use:
 keep.unique.title
 
@@ -175,7 +211,7 @@ summ.anim.type <- keep.unique.title %>%
   mutate(., Animal_Type = ifelse(str_detect(Animal_Type, ","),"Multiple",Animal_Type)) %>%
   #filter(str_detect(Animal_Type, "Livestock,"))
   summarise(n = n()) %>%
-  mutate(percent=n/131*100) %>%
+  mutate(percent=n/126*100) %>%
   arrange(., desc(percent)) %>%
   mutate(Animal_Type = factor(Animal_Type,
                               levels=c("Livestock", "Companion","Multiple",
@@ -221,15 +257,16 @@ animal.sp <- keep.syst.diff.ind.spp %>%
   summarise(n=sum(n)) %>%
   #arrange(desc(n)) %>% 
   mutate(Species_name = factor(Species_name)) %>%
-  mutate(percent=n/131*100) %>%
+  mutate(percent=n/126*100) %>%
   arrange(desc(percent))
+
 
 # Summarize by data provider
 summ.datprovider <- keep.unique.title %>%
   group_by(Data_provider) %>%
   summarise(n = n()) %>%
   filter(!is.na(Data_provider)) %>%
-  mutate(percent=n/131*100) %>%
+  mutate(percent=n/126*100) %>%
   mutate(., Data_provider = ifelse(Data_provider == "Multiple (see notes)", "Multiple",Data_provider)) %>%
   mutate(., Data_provider = ifelse(Data_provider == "Member of public", "Member public",Data_provider)) %>%
   mutate(., Data_provider = ifelse(Data_provider == "Private company (disposal services, etc)", 
@@ -238,7 +275,8 @@ summ.datprovider <- keep.unique.title %>%
   arrange(., desc(percent))
 
 # Summarize indicators
-# Note: here use 
+# Note: Here use to account for the use of different
+# indicators for different species within the same system
 keep.syst.diff.ind.spp
 
 # Indicators PER SYSTEM
@@ -256,13 +294,16 @@ summ.indicators <- keep.syst.diff.ind.spp %>%
   separate_rows(., Indicators_simpl, sep = ", ", convert = FALSE) %>%
   group_by(Indicators_simpl) %>%
   summarise(n = n()) %>%
-  mutate(percent=n/131*100) %>%
+  mutate(percent=n/126*100) %>%
   arrange(., desc(percent)) %>%
   mutate(., Indicators_simpl = ifelse(Indicators_simpl == "GI", 
                                       "gastrointestinal",Indicators_simpl))
 
 # Analytical approaches (spatial, temporal, spatio-temporal)
+# Note: Analysis at level of article since data from the same
+# system could be analyzed differently in each article
 analyt.dat <- dat.analysis2 %>%
+  filter(!(Source=="Gray literature")) %>%
   distinct(Title, .keep_all = TRUE)
 
 analysis.type <- analyt.dat %>%
@@ -288,7 +329,7 @@ world<-map_data("world")
 summ.country <- keep.unique.title %>%
   group_by(Country) %>%
   summarise(n = n()) %>%
-  mutate(percent=n/131*100) %>%
+  mutate(percent=n/126*100) %>%
   #mutate(., Country = ifelse(Country == "Korea South", "South Korea",Country)) %>%
   mutate(Systems = n) %>%
   # Remove systems in multiple countries, global, or no country
@@ -302,16 +343,17 @@ sys.map <- data.frame(world,data=sample(10,length(unique(world$group)),T)[world$
 
 # Plot the number of systems per country on a map (Figure 3)
 map.sys <- ggplot(sys.map)+
-  geom_polygon(aes(long,lat,group=group,fill=Systems), color="white", size = 0.2)+
-  scale_fill_viridis(begin = 0.6, direction=, option = "turbo", na.value="light grey") +
-  theme(panel.background = element_rect(fill = "lightsteelblue2"))
+  geom_polygon(aes(long,lat,group=group,fill=Systems), color="black", size = 0.35)+
+  scale_fill_viridis(begin = 0.6, direction=, option = "turbo", na.value="gray93") +
+  theme(panel.background = element_rect(fill = "lightsteelblue1"))
 # Save using ggsave if desired
+#######################################################################################
 
 ########################################################################################
 # Create sankey plots linking animal sector, data providers,
-# and indicators (Figure 4)
+# and indicators (Figure 4, Supplementary Figure 1)
 ########################################################################################
-# Use this data set:
+# Use this data set (created above in section "Indicators PER SYSTEM" , L. 284):
 ind.setup
 
 # Prepare and clean the data to create sankey plot
@@ -388,160 +430,4 @@ pl2 <- pl2 +geom_sankey(flow.alpha = 0.5
         panel.grid = element_blank())
 #Save with ggsave if desired
 
-########################################################################################
-# Multiple Factor Analysis
-########################################################################################
-# Start with:
-keep.syst.diff.ind.spp
-
-# Prepare the data for Multiple Factor Analysis
-m.dat.pre <- keep.syst.diff.ind.spp %>%
-  select(syst_id, Surveillance_Type,
-         Geographic_Extent, Country, Region.x, Animal_Type,
-         Data_provider, OneHealth, `Income group`, Num_Indicators_System) %>%
-  group_by(syst_id) %>%
-  mutate(Num_Indicators_System = sum(Num_Indicators_System)) %>%
-  distinct() %>%
-  ungroup() %>%
-  select(-syst_id)
-
-# Further data clean up
-m.dat.glob <- m.dat.pre %>%
-  select(Surveillance_Type,
-         Geographic_Extent, Region.x, Country, Animal_Type,
-         Data_provider, OneHealth, `Income group`, Num_Indicators_System) %>%
-  mutate(., Animal_Type = ifelse(str_detect(Animal_Type, ","),"Multiple Animal Sector",Animal_Type)) %>%
-  mutate(., Surveillance_Type = ifelse(Surveillance_Type=="Prospective/Proof of Concept",
-                                       "Proof of Concept",Surveillance_Type)) %>%
-  mutate(., Region.x = ifelse(Geographic_Extent=="Global",
-                              "Global",Region.x)) %>%
-  mutate(., OneHealth = ifelse(OneHealth == "Yes",
-                                             "OneHealth","Not OH")) %>%
-  mutate(., Data_provider = ifelse(Data_provider == "Multiple (see notes)", "Multiple data providers",Data_provider)) %>%
-  mutate(., Data_provider = ifelse(Data_provider == "Member of public", "Member public",Data_provider)) %>%
-  mutate(., Data_provider = ifelse(Data_provider == "Private company (disposal services, etc)", 
-                              "Private company",Data_provider)) %>%
-  mutate(., Data_provider = ifelse(Data_provider == "Member Study Team", "Study team",Data_provider)) %>%
-  mutate(., `Income group` = ifelse(`Income group` == "Low income", "Low/lower middle income",`Income group`)) %>%
-  mutate(., `Income group` = ifelse(`Income group` == "Lower middle income", "Low/lower middle income",`Income group`)) %>%
-  mutate(., `Income group` = ifelse(Geographic_Extent == "Multi-Country", "High income",`Income group`)) %>%
-  mutate(., `Income group` = ifelse(Country == "Global", "World",`Income group`)) %>%
-  select(-Country) %>%
-  mutate(`Income group` = ordered(factor(`Income group`,
-                                         levels=c("High income", "Upper middle income", 
-                                                  "Low/lower middle income", "World")))) %>%
-  drop_na(Geographic_Extent)
-
-# Perform Multiple Factor Analysis.
-# Use all 10 dimensions:
-res.mfa <- MFA(m.dat.glob, 
-               group = c(1,1,1,1,1,1,1,1), 
-               type = c("n", "n","n","n", "n", "n", "n","c"), ncp = 10,
-               name.group = c("system type","geographic scale", "continent",
-                              "animal sector", "Data_provider","onehealth", "income","number indicators"),
-               graph = T)
-# Note: Groups representation (Supplementary Figure 4) plots after running this
-# code with argument "graph = T"
-
-# Other MFA plots:
-# Scree plot (Supplementary Figure 2)
-scree <- fviz_screeplot(res.mfa, addlabels = TRUE)
-
-# Contribution of variables to the first dimension
-dim1.viz1 <- fviz_contrib(res.mfa, "group", axes = 1,title="")
-# Contribution of variables to the second dimension
-dim2.viz2 <- fviz_contrib(res.mfa, "group", axes = 2, title="")
-
-# Plot contributions to first and second dimensions in a single figure
-# (Supplementary Figure 3)
-dims <- ggpubr::ggarrange(dim1.viz1, dim2.viz2, ncol=2, nrow=1, labels = c("a.","b."))
-
-# Visualization -- Plotting first two dimensions, points / confidence intervals
-# colored according to different categorical variables:
-# Income level (Figure 5)
-income.vis.glob <- fviz_mfa_ind(res.mfa, geom = "point",
-                                habillage = m.dat.glob$`Income group`,#"Income group", # color by groups 
-                                #col.ind = m.dat.glob$`Income group`,
-                                palette = c("#0d0887", "#7e03a8", "#cc4778","#f89540"), #,"#f0f921"
-                                addEllipses = TRUE, ellipse.type = "confidence", 
-                                repel = TRUE # Avoid text overlapping
-                                ,title=""
-) 
-# Save with ggsave if desired
-
-# Visualizations for Supplementary Figure 5:
-# Continent
-continent.vis.glob <- fviz_mfa_ind(res.mfa, geom = "point",
-                                   habillage = as.factor(m.dat.glob$Region.x), 
-                                   #col.ind = m.dat.glob$`Income group`,
-                                   palette = c("#fdc527","#f89540","#e66c5c","#cc4778",
-                                               "#aa2395","#7e03a8","#4c02a1", "#0d0887"), #,"#f0f921"
-                                   addEllipses = TRUE, ellipse.type = "confidence", 
-                                   repel = TRUE # Avoid text overlapping
-                                   ,title="",
-                                   labels = NULL
-                                   
-) 
-
-# Sector
-sector.vis <- fviz_mfa_ind(res.mfa, 
-                           habillage = "Animal_Type", # color by groups 
-                           palette = c("#fcce25","#0d0887", "#7e03a8", "#cc4778","#f89540"),
-                           addEllipses = TRUE, ellipse.type = "confidence", 
-                           repel = TRUE, # Avoid text overlapping
-                           labels = NULL,
-                           geom = "point",
-                           title=""
-) 
-
-# OneHealth
-oh.vis <- fviz_mfa_ind(res.mfa, 
-                       habillage = "OneHealth", # color by groups 
-                       palette = c("#cc4778", "#0d0887"),
-                       addEllipses = TRUE, ellipse.type = "confidence", 
-                       repel = TRUE, # Avoid text overlapping,
-                       labels = NULL,
-                       geom = "point", title=""
-) 
-
-# Type of system
-typ.vis <- fviz_mfa_ind(res.mfa, 
-                        habillage = as.factor(m.dat.glob$Surveillance_Type), # color by groups 
-                        palette = c("#fcce25","#0d0887", "#7e03a8", "#cc4778","#f89540"),
-                        addEllipses = TRUE, ellipse.type = "confidence", 
-                        repel = TRUE, # Avoid text overlapping
-                        labels = NULL,
-                        geom = "point", title=""
-) 
-
-# Geographic extent
-geo.ext.vis <- fviz_mfa_ind(res.mfa, 
-                            habillage = as.factor(m.dat.glob$Geographic_Extent), # color by groups 
-                            palette = c("#fdc527","#f89540","#cc4778",
-                                        "#aa2395","#7e03a8", "#0d0887"),
-                            addEllipses = TRUE, ellipse.type = "confidence", 
-                            repel = TRUE, # Avoid text overlapping
-                            labels = NULL,
-                            geom = "point", title="",
-                            legend.partial.title= "Extent"
-) 
-
-
-# Data provider
-provider.vis <- fviz_mfa_ind(res.mfa, 
-                             habillage = as.factor(m.dat.glob$Data_provider), # color by groups 
-                             palette = c("#f0f921","#fcce25","#fca636",
-                                         "#f2844b","#e16462", "#cc4778",
-                                         "#b12a90","#8f0da4","#6a00a8",
-                                         "#41049d","#0d0887"),
-                             #addEllipses = TRUE, ellipse.type = "confidence", 
-                             repel = TRUE, # Avoid text overlapping
-                             labels = NULL,
-                             geom = "point", title="",
-                             legend.partial.title= "Extent"
-) 
-
-# Plot all together (Supplementary Figure 5)
-supp.vis <- ggpubr::ggarrange(typ.vis,geo.ext.vis, continent.vis.glob,sector.vis,provider.vis,oh.vis, ncol=2, nrow=3, 
-                              labels = c("a.","b.","c.","d.","e.", "f."))
-
+## END ##
